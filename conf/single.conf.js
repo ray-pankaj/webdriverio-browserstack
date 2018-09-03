244cd0b0004b91db6cd8e9c0bd86fbe750431177
Pankaj Ahuja <ray.pankaj@gmail.com>
2018-09-03 13:02:30 +0530

    added travis

--- conf/single.conf.js
+++ conf/single.conf.js
@@ -1,6 +1,6 @@
 exports.config = {
-  user: process.env.BROWSERSTACK_USERNAME || 'BROWSERSTACK_USERNAME',
-  key: process.env.BROWSERSTACK_ACCESS_KEY || 'BROWSERSTACK_ACC_KEY',
+  user: 'pankajahuja1',
+  key: 'aCxhDStUopf8tsyz4d55',
 
   updateJob: false,
   specs: [
